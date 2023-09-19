337c11dfb0973fa16ac38ec86d3320ea7d4994c3
Elsanose <h.elsanose@gmail.com>
2023-09-20 02:29:35 +0300

    prepare for deploy

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,5 +1,5 @@
 import { NavLink } from "react-router-dom";
-import style from "./pageNav.module.css";
+import style from "./PageNav.module.css";
 import Logo from "./Logo";
 const PageNav = () => {
   return (
